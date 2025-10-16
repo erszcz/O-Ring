c6541a9c68296420c36ee90a42c299c121d66290
Radek Szymczyszyn <lavrin@gmail.com>
2025-10-16 22:13:54 +0200

    Indent ring.erl

--- erlring/ring.erl
+++ erlring/ring.erl
@@ -14,12 +14,12 @@ start(N, M) ->
     buildRing(N),
     Head ! { rollcall, N, self() },
     receive 
-	allNodesUp -> StopRing = erlang:monotonic_time()
+        allNodesUp -> StopRing = erlang:monotonic_time()
     end,
     Start = erlang:monotonic_time(),
     Head ! N*M,
     receive
-	done -> Stop = erlang:monotonic_time()
+        done -> Stop = erlang:monotonic_time()
     end,
     RingCreation = erlang:convert_time_unit(StopRing-StartRing, native, millisecond),
     ElapsedTime = erlang:convert_time_unit(Stop-Start, native, millisecond),
@@ -29,7 +29,7 @@ start(N, M) ->
 prepareFirst(N) ->
     Head = spawn(?MODULE, initialize, [self()]),
     receive
-	{Head, ok} -> Head ! { forkNewProcess, N-1, Head, self()}
+        {Head, ok} -> Head ! { forkNewProcess, N-1, Head, self()}
     end,
     Head.
 
@@ -40,21 +40,21 @@ buildRing(N) -> setupRing(N, 1).
 setupRing(N, N) -> void;
 setupRing(N, K) ->
     receive 
-	ready -> setupRing(N, K+1)
+        ready -> setupRing(N, K+1)
     end.
 
 
 loop(Next, Parent) ->
     receive
         { rollcall, 0, Parent } -> 
-	    Parent ! allNodesUp,
-	    loop(Next, Parent);
+            Parent ! allNodesUp,
+            loop(Next, Parent);
         { rollcall, N, Parent } -> 
-	    Next ! { rollcall, N-1, Parent },
-	    loop(Next, Parent);
-	0 -> Parent ! done;
-	N -> Next ! (N-1),
-	     loop(Next, Parent)
+            Next ! { rollcall, N-1, Parent },
+            loop(Next, Parent);
+        0 -> Parent ! done;
+        N -> Next ! (N-1),
+             loop(Next, Parent)
     end.
 
 
@@ -65,7 +65,7 @@ prepare(0, OpenHead, Parent) ->
 prepare(N, OpenHead, Parent) ->
     Next = spawn(?MODULE, initialize, [self()]),
     receive
-	{Next, ok} -> Next ! { forkNewProcess, N-1, OpenHead, Parent}
+        {Next, ok} -> Next ! { forkNewProcess, N-1, OpenHead, Parent}
     end,
     Parent ! ready,
     loop(Next, Parent).
@@ -74,6 +74,6 @@ prepare(N, OpenHead, Parent) ->
 initialize(Previous) ->
     Previous ! {self(), ok},
     receive
-	{ forkNewProcess, N, OpenHead, Parent } ->
-	    prepare(N, OpenHead, Parent)
+        { forkNewProcess, N, OpenHead, Parent } ->
+            prepare(N, OpenHead, Parent)
     end.
